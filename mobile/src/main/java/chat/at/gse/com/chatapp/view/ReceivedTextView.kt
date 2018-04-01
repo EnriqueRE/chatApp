5acbd55f0b7f3afe9df9a6bbba50526a1b977c97
Enrique Ramirez <enrique.ramirez35@gmail.com>
2018-04-01 14:31:37 -0600

    Received text view
    view for displaying received texts

--- mobile/src/main/java/chat/at/gse/com/chatapp/view/ReceivedTextView.kt
+++ mobile/src/main/java/chat/at/gse/com/chatapp/view/ReceivedTextView.kt
@@ -1,4 +1,18 @@
 package chat.at.gse.com.chatapp.view
 
-class ReceivedTextView {
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import chat.at.gse.com.chatapp.R
+import chat.at.gse.com.chatapp.model.Message
+import kotlinx.android.synthetic.main.view_text_received_message.view.*
+
+class ReceivedTextView (view:View) : RecyclerView.ViewHolder(view) {
+    val imageProfile:ImageView = view.findViewById(R.id.image_message_profile)
+    val messageBody:TextView = view.findViewById(R.id.text_message_body)
+
+    fun bind(message:Message){
+        messageBody.text = message.messageBody.messagePayload.text
+    }
 }
