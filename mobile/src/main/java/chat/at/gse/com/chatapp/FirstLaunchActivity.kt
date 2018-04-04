5853432cceb05003b45877bef67d34d9568cdfd7
Enrique Ramirez <enrique.ramirez35@gmail.com>
2018-04-04 12:51:37 -0600

    First launch activity
    First launch activity created

--- mobile/src/main/java/chat/at/gse/com/chatapp/FirstLaunchActivity.kt
+++ mobile/src/main/java/chat/at/gse/com/chatapp/FirstLaunchActivity.kt
@@ -1,8 +1,10 @@
 package chat.at.gse.com.chatapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 import kotlinx.android.synthetic.main.activity_first_launch.*
 
@@ -11,12 +13,11 @@ class FirstLaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_launch)
-        setSupportActionBar(toolbar)
+    }
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+    fun startFlow(view:View){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 
 }
